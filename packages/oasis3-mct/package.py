6a61672cdb37a362327d71ac1e4b1da31505fdba
Harshula <harshula.jayasuriya@anu.edu.au>
2022-12-02 12:16:20 +1100

    Install oasis3-mct mod files in include
    * Use Spack's internal file manipulation functions.

--- packages/oasis3-mct/package.py
+++ packages/oasis3-mct/package.py
@@ -5,12 +5,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.package import *
-import sys
-import os
-import shutil
-from pathlib import Path
+from spack.package import install, join_path, mkdirp
 from pprint import pprint
+from itertools import repeat
 
 class Oasis3Mct(Package):
     """ACCESS-NRI's fork of https://gitlab.com/cerfacs/oasis3-mct OASIS3-MCT 2.0."""
@@ -18,7 +15,6 @@ class Oasis3Mct(Package):
     homepage = "https://www.access-nri.org.au"
     git = "https://github.com/ACCESS-NRI/oasis3-mct.git"
 
-    # notify when the package is updated.
     maintainers = ["harshula"]
 
     version("5e57b1aa40840adb6d6747d475dabadab5d9d6fb", commit="5e57b1aa40840adb6d6747d475dabadab5d9d6fb")
@@ -35,18 +31,121 @@ class Oasis3Mct(Package):
         build()
 
     def install(self, spec, prefix):
-        srcdir = os.path.join("compile_oa3-mct", "lib")
-        dstdir = os.path.join(prefix, "usr", "lib")
 
-        # TODO: Spack should have a way to find/create the installation path
-        Path(dstdir).mkdir(parents=True, exist_ok=True)
+        incs_mct = ["m_accumulatorcomms.mod",
+                    "m_accumulator.mod",
+                    "m_attrvectcomms.mod",
+                    "m_attrvect.mod",
+                    "m_attrvectreduce.mod",
+                    "m_chars.mod",
+                    "m_convertmaps.mod",
+                    "mct_mod.mod",
+                    "m_die.mod",
+                    "m_dropdead.mod",
+                    "m_errorhandler.mod",
+                    "m_exchangemaps.mod",
+                    "m_fccomms.mod",
+                    "m_filename.mod",
+                    "m_fileresolv.mod",
+                    "m_flow.mod",
+                    "m_generalgridcomms.mod",
+                    "m_generalgrid.mod",
+                    "m_globalmap.mod",
+                    "m_globalsegmapcomms.mod",
+                    "m_globalsegmap.mod",
+                    "m_globaltolocal.mod",
+                    "m_indexbin_char.mod",
+                    "m_indexbin_integer.mod",
+                    "m_indexbin_logical.mod",
+                    "m_inpak90.mod",
+                    "m_ioutil.mod",
+                    "m_list.mod",
+                    "m_mall.mod",
+                    "m_matattrvectmul.mod",
+                    "m_mctworld.mod",
+                    "m_merge.mod",
+                    "m_mergesorts.mod",
+                    "m_mpif90.mod",
+                    "m_mpif.mod",
+                    "m_mpout.mod",
+                    "m_navigator.mod",
+                    "m_permuter.mod",
+                    "m_rankmerge.mod",
+                    "m_realkinds.mod",
+                    "m_rearranger.mod",
+                    "m_router.mod",
+                    "m_sortingtools.mod",
+                    "m_sparsematrixcomms.mod",
+                    "m_sparsematrixdecomp.mod",
+                    "m_sparsematrix.mod",
+                    "m_sparsematrixplus.mod",
+                    "m_sparsematrixtomaps.mod",
+                    "m_spatialintegral.mod",
+                    "m_spatialintegralv.mod",
+                    "m_stdio.mod",
+                    "m_string.mod",
+                    "m_strtemplate.mod",
+                    "m_traceback.mod",
+                    "m_transfer.mod",
+                    "m_zeit.mod" ]
 
-        print(f"Installing files in {dstdir}", file=sys.stdout)
-        # TODO: See if there's a better way to do this?!
-        for f in ["libmct.a", "libmpeu.a", "libpsmile.MPI1.a", "libscrip.a"]:
-            srcpath = os.path.join(srcdir, f)
-            shutil.copy2(srcpath, dstdir)
+        incs_psmile = ["mod_oasis_advance.mod",
+                         "mod_oasis_coupler.mod",
+                         "mod_oasis_data.mod",
+                         "mod_oasis_getput_interface.mod",
+                         "mod_oasis_grid.mod",
+                         "mod_oasis_io.mod",
+                         "mod_oasis_ioshr.mod",
+                         "mod_oasis_kinds.mod",
+                         "mod_oasis_method.mod",
+                         "mod_oasis.mod",
+                         "mod_oasis_mpi.mod",
+                         "mod_oasis_namcouple.mod",
+                         "mod_oasis_parameters.mod",
+                         "mod_oasis_part.mod",
+                         "mod_oasis_string.mod",
+                         "mod_oasis_sys.mod",
+                         "mod_oasis_timer.mod",
+                         "mod_oasis_var.mod",
+                         "mod_prism.mod"]
 
-        #print(os.environ)
-        #pprint(dir(spec))
-        #pprint(spec.__dict__, indent=2)
+        incs_scrip = [ "constants.mod",
+                        "grids.mod",
+                        "iounits.mod",
+                        "kinds_mod.mod",
+                        "mod_oasis_flush.mod",
+                        "netcdf_mod.mod",
+                        "remap_bicubic.mod",
+                        "remap_bicubic_reduced.mod",
+                        "remap_bilinear.mod",
+                        "remap_bilinear_reduced.mod",
+                        "remap_conservative.mod",
+                        "remap_distance_weight.mod",
+                        "remap_gaussian_weight.mod",
+                        "remap_vars.mod",
+                        "remap_write.mod",
+                        "timers.mod" ]
+
+
+        incs = [join_path("mct", f) for f in incs_mct] \
+                + [join_path("psmile.MPI1", f) for f in incs_psmile] \
+                + [join_path("scrip", f) for f in incs_scrip]
+
+        libs = ["libmct.a", "libmpeu.a", "libpsmile.MPI1.a", "libscrip.a"]
+
+        incsrcdir = join_path("compile_oa3-mct", "build", "lib")
+        libsrcdir = join_path("compile_oa3-mct", "lib")
+
+        incdstdir = self.prefix.include
+        mkdirp(join_path(incdstdir, "mct"))
+        mkdirp(join_path(incdstdir, "psmile.MPI1"))
+        mkdirp(join_path(incdstdir, "scrip"))
+
+        libdstdir = self.prefix.lib
+        mkdirp(libdstdir)
+
+        inc_list_of_tuples = [(join_path(incsrcdir, f), join_path(incdstdir, f)) for f in incs]
+        lib_list_of_tuples = [(join_path(libsrcdir, f), join_path(libdstdir, f)) for f in libs]
+
+        [install(s,d) for (s,d) in inc_list_of_tuples + lib_list_of_tuples]
+
