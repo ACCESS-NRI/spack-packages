2f106b98fed630c5f211a221e6bbc7a70cf559cb
Harshula <harshula.jayasuriya@anu.edu.au>
2022-12-09 15:34:17 +1100

    Generate and install oasis3-mct library pkgconfig files

--- packages/oasis3-mct/package.py
+++ packages/oasis3-mct/package.py
@@ -7,7 +7,7 @@
 
 from spack.package import install, join_path, mkdirp
 from pprint import pprint
-from itertools import repeat
+import inspect
 
 class Oasis3Mct(Package):
     """ACCESS-NRI's fork of https://gitlab.com/cerfacs/oasis3-mct OASIS3-MCT 2.0."""
@@ -20,9 +20,51 @@ class Oasis3Mct(Package):
     version("5e57b1aa40840adb6d6747d475dabadab5d9d6fb", commit="5e57b1aa40840adb6d6747d475dabadab5d9d6fb")
 
     depends_on("netcdf-fortran@4.5.2:")
+    # TODO: Should we depend on virtual package "mpi" instead?
     depends_on("openmpi@4.0.2:")
 
     phases = ["build", "install"]
+
+    # TODO: Implement a determine_version(cls, exe)
+
+    __builddir = "compile_oa3-mct"
+    __incdir = join_path(__builddir, "build", "lib")
+    __libdir = join_path(__builddir, "lib")
+    __pkgdir = join_path(__libdir, "pkgconfig")
+    __libs = {"mct": {}, "mpeu": {}, "psmile.MPI1": {}, "scrip": {}}
+
+
+    def __create_pkgconfig(self, spec, prefix):
+
+        mkdirp(self.__pkgdir)
+
+        for k in self.__libs.keys():
+            self.__libs[k]["filename"] = "lib" + k + ".a"
+            self.__libs[k]["pcname"] = k + ".pc"
+            self.__libs[k]["pcrelpath"] = join_path("pkgconfig", self.__libs[k]["pcname"])
+            self.__libs[k]["pcpath"] = join_path(self.__pkgdir, self.__libs[k]["pcname"])
+            self.__libs[k]["pctext"] = f"""\
+prefix={prefix}
+exec_prefix=${{prefix}}
+libdir=${{exec_prefix}}/lib
+includedir=${{prefix}}/include
+
+Name: {k}
+Description: OASIS3-MCT 2.0 {k} Library for Fortran
+Version: 2.0
+Libs: -L${{libdir}} -l{k}
+Cflags: -I${{includedir}}
+"""
+            text = self.__libs[k]["pctext"]
+
+            with open(self.__libs[k]["pcpath"], "w", encoding="utf-8") as pc:
+                nchars_written = pc.write(text)
+
+            if nchars_written < len(text):
+                raise OSError
+
+        #pprint(self.__libs)
+
 
     def build(self, spec, prefix):
         # TODO: Need to rethink the build process. Likely require modifying
@@ -30,7 +72,22 @@ class Oasis3Mct(Package):
         build = Executable("./build.sh")
         build()
 
+        # Upstream is missing a pkgconfig files, so we'll create them.
+        self.__create_pkgconfig(spec, prefix)
+
+
     def install(self, spec, prefix):
+
+        # doc/oasis3mct_UserGuide.pdf:
+        # If module mod_oasis is used in the models, it is enough to include
+        # the path of the psmile objects and modules
+        # ($ARCHDIR/build/lib/psmile.MPI1) for the compilation and to use the
+        # psmile library $ARCHDIR/lib/libpsmile.MPI1.a when linking.
+        # If module mod_prism is used in the models, it is necessary to include
+        # the path of the psmile and of the mct objects and modules for the
+        # compilation (i.e. $ARCHDIR/build/lib/psmile.MPI1 and /mct and to
+        # use both the psmile and mct libraries $ARCHDIR/lib/libpsmile.MPI1.a
+        # and libmct.a and libmpeu.a when linking.
 
         incs_mct = ["m_accumulatorcomms.mod",
                     "m_accumulator.mod",
@@ -131,21 +188,21 @@ class Oasis3Mct(Package):
                 + [join_path("psmile.MPI1", f) for f in incs_psmile] \
                 + [join_path("scrip", f) for f in incs_scrip]
 
-        libs = ["libmct.a", "libmpeu.a", "libpsmile.MPI1.a", "libscrip.a"]
+        libs = [self.__libs[k]["filename"] for k in self.__libs.keys()] \
+                + [self.__libs[k]["pcrelpath"] for k in self.__libs.keys()]
 
-        incsrcdir = join_path("compile_oa3-mct", "build", "lib")
-        libsrcdir = join_path("compile_oa3-mct", "lib")
-
-        incdstdir = self.prefix.include
-        mkdirp(join_path(incdstdir, "mct"))
+        incdstdir = prefix.include
+        mkdirp(prefix.include.mct)
+        # The directory name has a '.', so create the path manually:
         mkdirp(join_path(incdstdir, "psmile.MPI1"))
-        mkdirp(join_path(incdstdir, "scrip"))
+        mkdirp(prefix.include.scrip)
 
-        libdstdir = self.prefix.lib
+        libdstdir = prefix.lib
         mkdirp(libdstdir)
+        mkdirp(prefix.lib.pkgconfig)
 
-        inc_list_of_tuples = [(join_path(incsrcdir, f), join_path(incdstdir, f)) for f in incs]
-        lib_list_of_tuples = [(join_path(libsrcdir, f), join_path(libdstdir, f)) for f in libs]
+        inc_list_of_tuples = [(join_path(self.__incdir, f), join_path(incdstdir, f)) for f in incs]
+        lib_list_of_tuples = [(join_path(self.__libdir, f), join_path(libdstdir, f)) for f in libs]
 
         [install(s,d) for (s,d) in inc_list_of_tuples + lib_list_of_tuples]
 
