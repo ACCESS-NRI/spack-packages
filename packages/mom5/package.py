bb9c75d749e830bea4b6b1fe989bab365cb7bcee
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2023-03-22 12:45:50 +1100

    mom5: change branch dependency from "spack-build" to "master" for merge
    * Do not depend on hdf5 because netcdf-c already does.
    * https://github.com/ACCESS-NRI/spack_packages/pull/7

--- packages/mom5/package.py
+++ packages/mom5/package.py
@@ -16,7 +16,7 @@ class Mom5(MakefilePackage):
 
     maintainers = ["harshula"]
 
-    version("spack-build", branch="spack-build")
+    version("master", branch="master")
 
     # Depend on virtual package "mpi".
     depends_on("mpi")
@@ -29,7 +29,6 @@ class Mom5(MakefilePackage):
     # ~shared requires: https://github.com/spack/spack/pull/34837
     depends_on("parallelio~pnetcdf~timing~shared")
     depends_on("libaccessom2")
-    depends_on("hdf5")
 
     phases = ["edit", "build", "install"]
 
@@ -42,15 +41,19 @@ class Mom5(MakefilePackage):
         makeinc_path = join_path(srcdir, "bin", "mkmf.template.spack")
         config = {}
 
-        incs = (spec["oasis3-mct"].headers).cpp_flags + "/psmile.MPI1" + " "
-        for l in ["parallelio", "oasis3-mct", "libaccessom2", "netcdf-fortran"]:
-            incs += (spec[l].headers).cpp_flags + " "
+        istr = join_path((spec["oasis3-mct"].headers).cpp_flags, "psmile.MPI1")
+        ideps = ["parallelio", "oasis3-mct", "libaccessom2", "netcdf-fortran"]
+        incs = " ".join([istr] + [(spec[d].headers).cpp_flags for d in ideps])
 
+        lstr = " ".join(
+                    ["".join(["-L", join_path(spec["parallelio"].prefix, "lib")]),
+                    "-lpiof",
+                    "-lpioc"]
+                   )
         # NOTE: The order of the libraries matter during the linking step!
         # NOTE: datetime-fortran is a dependency of libaccessom2.
-        libs = "-L" + (spec["parallelio"].prefix) + "/lib -lpiof -lpioc "
-        for l in ["oasis3-mct", "libaccessom2", "netcdf-c", "netcdf-fortran", "datetime-fortran"]:
-            libs += (spec[l].libs).ld_flags + " "
+        ldeps = ["oasis3-mct", "libaccessom2", "netcdf-c", "netcdf-fortran", "datetime-fortran"]
+        libs = " ".join([lstr] + [(spec[d].libs).ld_flags for d in ldeps])
 
         # Copied from bin/mkmf.template.ubuntu
         config["gcc"] = f"""
@@ -295,14 +298,6 @@ TMPFILES = .*.m *.T *.TT *.hpm *.i *.lst *.proc *.s
         # cd ${ACCESS_OM_DIR}/src/mom/exp
         # export mom_type=ACCESS-OM
         # ./MOM_compile.csh --type $mom_type --platform spack
-
-        # cppDefs = "-Duse_netCDF -Duse_netCDF3 -Duse_libMPI -DUSE_OCEAN_BGC -DENABLE_ODA -DSPMD -DLAND_BND_TRACERS"
-        # set executable    = $root/exec/$platform/$type/fms_$type.x
-        # set srcList = ( accessom_coupler )
-        # set includes = "-I$executable:h:h/lib_FMS -I$executable:h:h/$type/lib_ocean"
-        # set libs = "$executable:h:h/$type/lib_ocean/lib_ocean.a $executable:h:h/lib_FMS/lib_FMS.a"
-        # bin/mkmf -f -m Makefile -a src -t bin/mkmfs.template.spack -p $executable:t" -o "$includes" -c "$cppDefs" -l "$libs"  $srcList
-
         with working_dir(join_path(self.stage.source_path, "exp")):
             build = Executable("./MOM_compile.csh")
             build(
