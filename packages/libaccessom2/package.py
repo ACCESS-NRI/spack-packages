636a8f59997bd6e1fcfd2083b880420af42567b9
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2023-05-01 16:43:15 +1000

    libaccessom2: add a variant for deterministic builds and build_type

--- packages/libaccessom2/package.py
+++ packages/libaccessom2/package.py
@@ -17,14 +17,25 @@ class Libaccessom2(CMakePackage):
 
     version("master", branch="master")
 
+    variant("deterministic", default=False, description="Deterministic build.")
+    variant('build_type',
+            default='Release',
+            description='The build type to build',
+            values=('Debug', 'Release')
+    )
+
     depends_on("cmake@3.20:", type="build")
     depends_on("pkgconf", type="build")
     # Depend on virtual package "mpi".
     depends_on("mpi")
-    depends_on("oasis3-mct")
+    depends_on("oasis3-mct+deterministic", when="+deterministic")
+    depends_on("oasis3-mct~deterministic", when="~deterministic")
     depends_on("datetime-fortran")
     depends_on("json-fortran")
     depends_on("netcdf-fortran@4.5.2:")
 
     def url_for_version(self, version):
         return "https://github.com/ACCESS-NRI/libaccessom2/tarball/{0}".format(version)
+
+    def cmake_args(self):
+        return [self.define_from_variant('DETERMINISTIC', "deterministic")]
