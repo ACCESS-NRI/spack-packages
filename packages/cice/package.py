79d34bbb1f08d5cc01f4e993a19b577e67567fb0
anton-seaice <anton.steketee@anu.edu.au>
2024-12-06 16:19:16 +1100

    increments

--- packages/cice/package.py
+++ packages/cice/package.py
@@ -2,22 +2,6 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install cice
-#
-# You can edit this file again by typing:
-#
-#     spack edit cice
-#
-# See the Spack documentation for more information on packaging.
 # ----------------------------------------------------------------------------
 
 from spack.package import *
@@ -27,6 +11,7 @@ class Cice(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
     homepage = "https://github.com/CICE-Consortium/CICE"
+    url = "https://github.com/CICE-Consortium/CICE/archive/refs/tags/CICE6.6.0.tar.gz"
     git = "https://github.com/ACCESS-NRI/CICE"
     submodules = True
     maintainers = ["anton-seaice", "harshula"]
@@ -45,43 +30,36 @@ class Cice(CMakePackage):
         )
     )
 
-    variant("openmp", default=False, description="Enable OpenMP")
-    
-    
-    cesmcoupled = variant(
-        "cesmcoupled", 
-        default=False,
-        values=(
-            True, False, 
-            conditional(True, when="+access3")
-        ),
-        description="Set CESMCOUPLED CPP Flag"
+    variant("io_type", 
+        default = "NetCDF" ,
+        values = ("NetCDF", "PIO", "Binary"),
+        description = "CICE IO Method"
     )
 
-    # depends
+    variant("openmp", default=False, description="Enable OpenMP")
+    variant("cesmcoupled", default=False, description="Set CESMCOUPLED CPP Flag")
 
-    # set_variant
-    variant("access3", default=False, description="Link to Access3 share")
-    
     depends_on("cmake@3.18:", type="build")
     depends_on("mpi")
-    depends_on("netcdf-fortran@4.6.0:")
-    
-    depends_on("parallelio@2.5.10: build_type==RelWithDebInfo")
-    depends_on("parallelio fflags='-qno-opt-dynamic-align -convert big_endian -assume byterecl -ftz -traceback -assume realloc_lhs -fp-model source' cflags='-qno-opt-dynamic-align -fp-model precise -std=gnu99'", when="%intel")
 
-    depends_on("access3-share+install_libraries", when="+access3")
-    depends_on("access3-share+openmp+install_libraries", when="+access3+openmp")
+    depends_on("netcdf-fortran@4.6.0:", when="io_type=NetCDF")
+    depends_on("parallelio@2.5.10: build_type==RelWithDebInfo", when="io_type=PIO")
+    depends_on("parallelio fflags='-qno-opt-dynamic-align -convert big_endian -assume byterecl -ftz -traceback -assume realloc_lhs -fp-model source' cflags='-qno-opt-dynamic-align -fp-model precise -std=gnu99'", when="%intel io_type=PIO")
+
+    depends_on("access3-share+install_libraries", when="driver=nuopc/cmeps")
+    depends_on("access3-share+openmp+install_libraries", when="+openmp driver=nuopc/cmeps")
 
     root_cmakelists_dir = "cmake"
     
     def cmake_args(self):
         args = [
-            self.define("OM3_CICE_IO", "PIO"),
-            self.define_from_variant("OM3_LIB_INSTALL", "install_libraries"),
-            self.define_from_variant("OM3_OPENMP", "openmp"),
+            self.define_from_variant("CICE_IO", "io_type"),
+            self.define_from_variant("OPENMP", "openmp"),
             self.define_from_variant("CESMCOUPLED", "cesmcoupled")
         ]
+
+        if self.spec.satisfies("driver=nuopc/cmeps"):
+            args.append(self.define("ACCESS3_LIB_INSTALL", True))
 
         # we need this for cmake to find MPI_Fortran
         args.append(self.define("CMAKE_C_COMPILER", self.spec["mpi"].mpicc))
