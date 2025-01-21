2a4b0f1db91da52f5de8af5d64caf57ec92c64a2
Anton Steketee <79179784+anton-seaice@users.noreply.github.com>
2025-01-21 17:00:46 +1100

    Missing fms precision
    Copied from access-om3-nuopc package

--- packages/access-mom6/package.py
+++ packages/access-mom6/package.py
@@ -41,6 +41,7 @@ class AccessMom6(CMakePackage):
     depends_on("cmake@3.18:", type="build")
     depends_on("mpi")
     depends_on("netcdf-fortran@4.6.0:")
+    depends_on("fms@2021.03: build_type==RelWithDebInfo precision=64 +large_file ~gfs_phys ~quad_precision")
     depends_on("fms +openmp", when="+openmp")
     depends_on("fms ~openmp", when="~openmp")
 
