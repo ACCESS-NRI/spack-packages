c97ce7c90efb60a3ee76bd4841fb50a1c21c089d
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2025-03-06 15:02:48 +1100

    Add package recipe for ACCESS-TEST-model (#206)
    * Add package recipe for ACCESS-TEST-model
    ---------
    Co-authored-by: Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>

---
+++ packages/access-test-model/package.py
@@ -0,0 +1,30 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# Based on spack/var/spack/repos/builtin/packages/fiat/package.py
+
+from spack.package import *
+
+
+class AccessTestModel(CMakePackage):
+    """ACCESS Test Model is a small test FORTRAN model component
+       used to test CI workflows."""
+
+    homepage = "https://github.com/ACCESS-NRI/access-test-model"
+    git = "https://github.com/ACCESS-NRI/access-test-model.git"
+
+    maintainers("aidanheerdegen", "codegat", "harshula")
+
+    license("Apache-2.0")
+
+    version("main", branch="main", no_cache=True)
+
+    variant("mpi", default=True, description="Use MPI")
+
+    depends_on("mpi", when="+mpi")
+
+    root_cmakelists_dir = "stub"
+
+    def url_for_version(self, version):
+        return "https://github.com/ACCESS-NRI/access-test-model/tarball/{0}".format(version)
