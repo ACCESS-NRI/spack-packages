3f5fb7d0293bdc528b17e38e398c521b32122f99
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2025-03-12 17:00:14 +1100

    removed lines used for debugging

--- packages/access-triangle/package.py
+++ packages/access-triangle/package.py
@@ -68,9 +68,6 @@ class AccessTriangle(MakefilePackage):
         mkdirp(prefix.lib)
 
         with working_dir(src):
-            # Make sure we see what's actually there, for debugging:
-            ls_output = Executable('ls')('-l', '.', output=str, error=str)
-            print("Files in build directory:\n", ls_output)
             
             install('triangle.h', prefix.include)
 
