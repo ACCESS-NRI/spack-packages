d48948e20023ed86c93962932a02479d5466586b
anton-seaice <anton.steketee@anu.edu.au>
2025-03-26 17:02:49 +1100

    some variable name changes

--- packages/access-ww3/package.py
+++ packages/access-ww3/package.py
@@ -24,13 +24,8 @@ class AccessWw3(CMakePackage):
 
     def cmake_args(self):
         args = [
-<<<<<<< Updated upstream
-            self.define_from_variant("OPENMP", "openmp"),
-            self.define_from_variant("ACCESS3_WW3", "access3"),
-=======
             self.define_from_variant("WW3_OPENMP", "openmp"),
             self.define_from_variant("WW3_ACCESS3", "access3"),
->>>>>>> Stashed changes
         ]
 
         return args
