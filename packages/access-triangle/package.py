a22bf549173313a28339e638c3eb4799c25535ab
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2025-03-26 12:05:51 +1100

    removed redundancies

--- packages/access-triangle/package.py
+++ packages/access-triangle/package.py
@@ -1,34 +1,32 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-# ----------------------------------------------------------------------------
-#
-#     spack install access-triangle-git
-#
-# You can edit this file again by typing:
-#
-#     spack edit access-triangle-git
-#
-# ----------------------------------------------------------------------------
+
 
 from spack.package import *
-import glob
-import os
+# import os
 
 
 class AccessTriangle(MakefilePackage):
-    """Spack package for the ISSM Triangle library."""
+    """Triangle is a two-dimensional mesh generator and Delaunay
+    triangulator. Triangle generates exact Delaunay triangulations,
+    constrained Delaunay triangulations, conforming Delaunay
+    triangulations, Voronoi diagrams, and high-quality triangular
+    meshes."""
 
     homepage = "https://github.com/ACCESS-NRI/issm-triangle"
     git = 'https://github.com/ACCESS-NRI/issm-triangle.git'
+    
+    maintainers("justinh2002")
 
-    version('main')
+    version("1.6.1", branch="main")
+    
+    # variant for building the showme utility (requires X11).
+    variant("showme", default=False,
+            description="Build the showme utility (requires libX11).")
 
-    # # If on some systems 'gmake' is truly required, keep it. Otherwise, 'make'
-    # # is often sufficient because Spack sets MAKE appropriately.
-    depends_on('gmake',  type='build')
-    depends_on('libx11', type='link')
+    # Make libX11 conditional on +showme
+    depends_on("libx11", when="+showme", type="link")
     
     def url_for_version(self, version):
         return "https://github.com/ACCESS-NRI/issm-triangle/archive/refs/heads/{0}.tar.gz".format(version)
@@ -36,27 +34,27 @@ class AccessTriangle(MakefilePackage):
     def edit(self, spec, prefix):
         """
         Below, we just copy in the 'configs'
-        so that the build can find them in `self.build_directory`.
+        so that the build can find them in `src_dir`.
         """
         src_dir = join_path(self.stage.source_path, "src")
         mkdirp(src_dir)
         
         # Copy necessary files to src directory
-        install('configs/makefile', src_dir)
-        install('configs/linux/configure.make', src_dir)
-        install('triangle.c', src_dir)
-        install('triangle.h', src_dir)
+        install("configs/makefile", src_dir)
+        install("configs/linux/configure.make", src_dir)
+        install("triangle.c", src_dir)
+        install("triangle.h", src_dir)
         
     def build(self, spec, prefix):
         """
         This is where we actually call `make shared`.
-        Using MakefilePackage, you *could* rely on build_targets,
-        but we'll be explicit here so you can see it clearly.
+        Using MakefilePackage, you *could* rely on build_targets
         """
         with working_dir(join_path(self.stage.source_path, "src")):
-            make('shared')
+            make("shared")
+            if "+showme" in spec:
+                make("showme")
             
-        #raise an exception
 
     def install(self, spec, prefix):
         """
@@ -70,13 +68,15 @@ class AccessTriangle(MakefilePackage):
 
         with working_dir(src):
             # Make sure we see what's actually there, for debugging:
-            ls_output = Executable('ls')('-l', '.', output=str, error=str)
-            print("Files in build directory:\n", ls_output)
+            # ls_output = os.listdir('.')
+            # print("Files in build directory:\n", ls_output)
             
-            install('triangle.h', prefix.include)
-
-            for libfile in glob.glob("libtriangle.*"):
-                install(libfile, prefix.lib)
+            install("triangle.h", prefix.include)
+            install("libtriangle.so", prefix.lib)
+            
+            # Install showme only if +showme is chosen
+            if "+showme" in spec:
+                install("showme", prefix.bin)
 
             
 
