e2bdb46e56af8ac14183e7ed25da9235486c973a
Tommy Gatti <tommy.gatti@anu.edu.au>
2025-04-02 12:03:25 +1100

    Add access-issm bundle package (#218)

---
+++ packages/access-issm/package.py
@@ -0,0 +1,26 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# Copyright 2025 ACCESS-NRI
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import BundlePackage, maintainers, version, depends_on
+
+
+class AccessIssm(BundlePackage):
+    """
+    ACCESS-ISSM bundle containing the issm Package.
+
+    This is used to version the entirety of a released deployment, including
+    the package, it's dependencies, and the version of
+    spack-packages/spack-config that it is bundled with
+    """
+
+    homepage = "https://www.access-nri.org.au"
+    git = "https://github.com/ACCESS-NRI/ACCESS-ISSM.git"
+
+    maintainers("harshula", "justinh2002")
+
+    version("latest")
+
+    depends_on("issm")
