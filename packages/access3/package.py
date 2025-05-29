d22e3e5930258b5fa581508cb7656d772139204c
Spencer Wong <spencer.wong@anu.edu.au>
2025-05-29 13:59:13 +1000

    Add MOM6-CICE6-UM13 configuration for gcom control

--- packages/access3/package.py
+++ packages/access3/package.py
@@ -13,6 +13,7 @@ KNOWN_CONF = (
     "MOM6-CICE6",
     "CICE6-WW3",
     "MOM6-CICE6-WW3",
+    "MOM6-CICE6-UM13"
 )
 
 class Access3(CMakePackage):
@@ -40,12 +41,6 @@ class Access3(CMakePackage):
         ),
         sticky=True  # force concretizer to not pick alternative variants
     )
-    variant("cm3",
-            default=False,
-            description=(
-                "Build for ACCESS-CM3. Builds additional dependencies when True."
-            )
-            )
 
     # force user to supply a build combination
     conflicts(
@@ -57,7 +52,6 @@ class Access3(CMakePackage):
     depends_on("mpi")
     depends_on("access3-share")
     depends_on("esmf@8.7.0:")
-    depends_on("gcom@8.0", when="+cm3")
 
     for conf in KNOWN_CONF:
         if "CICE6" in conf:
@@ -66,6 +60,8 @@ class Access3(CMakePackage):
             depends_on("access-mom6+access3", when=f"configurations={conf}")
         if "WW3" in conf:
             depends_on("access-ww3+access3", when=f"configurations={conf}")
+        if "UM13" in conf:
+            depends_on("gcom@8.0", when=f"configurations={conf}")
 
     def cmake_args(self):
         # make configurations a cmake argument
