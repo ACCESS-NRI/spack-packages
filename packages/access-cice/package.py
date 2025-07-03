04b64d26c9dfe86fd6fd41571acd6c5dd4e02c4f
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2025-07-03 17:45:36 +1000

    Revert "access-om3 CMake SPRs: Use build_system_flags as the flag handler"
    This reverts commit 819a2e8d8754863c312e9becf1c81bad21df11cc.

--- packages/access-cice/package.py
+++ packages/access-cice/package.py
@@ -42,8 +42,6 @@ class AccessCice(CMakePackage):
 
     root_cmakelists_dir = "configuration/scripts/cmake"
 
-    flag_handler = build_system_flags
-
     def cmake_args(self):
         args = [
             self.define_from_variant("CICE_OPENMP", "openmp"),
