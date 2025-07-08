f8e69c143c4affa01a64e02a6f32a72b9918d614
Tommy Gatti <tommy.gatti@anu.edu.au>
2025-07-08 14:13:36 +1000

    Add CoastRI-ROMS SBR

---
+++ packages/coastri-roms/package.py
@@ -0,0 +1,24 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class CoastriRoms(BundlePackage):
+    """ROMS is a free-surface, terrain-following,
+    primitive equations ocean model widely used by
+    the scientific community for a diverse range of applications
+
+    This bundle package used to version the entirety of a released deployment, including
+    the package, it's dependencies, and the version of
+    spack-packages/spack-config that it is bundled with
+    """
+
+    homepage = "https://www.access-nri.org.au"
+    git = "https://github.com/ACCESS-NRI/CoastRI-ROMS.git"
+
+    version("latest")
+
+    depends_on("ancoms-roms")
