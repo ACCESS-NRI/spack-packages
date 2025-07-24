56fa279f3dda4f98c18e27b0d1561759eb4cdd86
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2025-07-24 23:33:08 +1000

    access_mocsy, access3_share, mom5: fix namespace

--- packages/access3_share/package.py
+++ packages/access3_share/package.py
@@ -4,7 +4,7 @@
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack.package import *
-from spack.pkg.access.nri.access3 import ACCESS3_VERSIONS
+from spack_repo.access.nri.packages.access3.package import ACCESS3_VERSIONS
 
 class Access3Share(CMakePackage):
     """Shared coupler/mediator libraries used by the ACCESS version 3 climate
--- packages/mom5/package.py
+++ packages/mom5/package.py
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.build_systems import cmake, makefile
+from spack_repo.builtin.build_systems import cmake, makefile
 from spack.version.version_types import GitVersion, StandardVersion
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack_repo.builtin.build_systems.makefile import MakefilePackage
