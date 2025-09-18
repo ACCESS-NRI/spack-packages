795013ea077d44d7af4be58cb30d3d8bc75a08b5
Spencer Wong <spencer.wong@anu.edu.au>
2025-09-18 17:05:44 +1000

    Remove UM13 build configuration

--- packages/access3/package.py
+++ packages/access3/package.py
@@ -13,7 +13,6 @@ KNOWN_CONF = (
     "MOM6-CICE6",
     "CICE6-WW3",
     "MOM6-CICE6-WW3",
-    "MOM6-CICE6-UM13",
 )
 
 class Access3(CMakePackage):
@@ -65,8 +64,6 @@ class Access3(CMakePackage):
             depends_on("access-mom6@2025.02.000: +access3", when=f"configurations={conf}")
         if "WW3" in conf:
             depends_on("access-ww3@2025.03.0: +access3", when=f"configurations={conf}")
-        if "UM13" in conf:
-            depends_on("gcom@8.0", when=f"configurations={conf}")
 
     flag_handler = build_system_flags
 
