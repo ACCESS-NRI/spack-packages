d8558a368fcac17ff9e291db1d1918f22c32de5e
Lawrence Bird <lawrence.bird@anu.edu.au>
2025-10-27 15:51:04 +1100

    Update petsc / mumps depends

--- packages/issm/package.py
+++ packages/issm/package.py
@@ -84,8 +84,8 @@ class Issm(AutotoolsPackage):
 
     # Linear-algebra stack - only for the *non-AD* flavour
     # depends_on("petsc~examples+metis+mumps+scalapack", when="~ad")
-    depends_on("petsc~examples, when="~ad")
-    depends_on("metris+mumps+scalapack", when="~ad")
+    depends_on("petsc~examples", when="~ad")
+    depends_on("metis+scalapack", when="~ad") #Removing mumps here because it's added below. 
     depends_on("parmetis")
     depends_on("metis")
     depends_on("mumps~openmp", when="~openmp")
